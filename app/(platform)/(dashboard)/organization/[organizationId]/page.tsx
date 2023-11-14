189cc15c58a8fa2692eff1196a08c842a8807e5d
RiP3rQ <rafalpompa2000@gmail.com>
2023-11-14 16:07:05 +0100

    SERVER ACTIONS with NEXTJS 14 (custom useAction hook) IMPORTANT

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
 
-const OrganizationIdPage = async () => {
-  return <div>OrganizationPage</div>;
+import { createBoard } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/use-action";
+
+const OrganizationIdPage = () => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error);
+      console.log(fieldErrors);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({ title });
+  };
+
+  return (
+    <div>
+      <form action={onSubmit}>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          placeholder="Enter board name!"
+          className="border-black border p-1"
+        />
+        <Button type="submit">Create</Button>
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;

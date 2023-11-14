4f0dc3290c3249935f24ee8606eba8b25a7c4ce0
RiP3rQ <rafalpompa2000@gmail.com>
2023-11-14 17:43:38 +0100

    unsplash integration + form-picker component

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -17,6 +17,7 @@ import { createBoard } from "@/actions/create-board";
 
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
+import { FormPicker } from "./form-picker";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -73,10 +74,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className="space-y-4">
           <div className="space-y-4">
-            {/* <FormPicker
-              id="image"
-              errors={fieldErrors}
-            /> */}
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board title"
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["img.clerk.com"],
+    domains: ["img.clerk.com", "images.unsplash.com"],
   },
 };
 
